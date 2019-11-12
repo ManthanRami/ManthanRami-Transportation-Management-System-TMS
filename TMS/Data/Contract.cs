a9dd8ae37b1ebab926dc466c335b57580a65d5c9
Sniddunc <sniddunc2@gmail.com>
2019-11-12 17:18:45 -0500

    :building_construction: Created CmpDal, TmsDal, User and Contract classes

---
+++ TMS/Data/Contract.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Data
+{
+    public enum City
+    {
+        Windsor = 0,
+        London = 1,
+        Hamilton = 2,
+        Toronto = 3,
+        Oshawa = 4,
+        Belleville = 5,
+        Kingston = 6,
+        Ottawa = 7
+    }
+
+    public enum JobType
+    {
+        FTL = 0,
+        LTL = 1
+    }
+
+    public enum VanType
+    {
+        Dry = 0,
+        Reefer = 1
+    }
+
+    class Contract
+    {
+        private String Client { get; set; }
+
+        private JobType JobType { get; set; }
+        private VanType VanType { get; set; }
+
+        private int Quantity { get; set; }
+
+        private City origin;
+        private City destination;
+    }
+}
---
+++ TMS/Data/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Data
+{
+    public enum UserType
+    {
+        Planner = 0,
+        Buyer = 1,
+        Admin = 2
+    }
+
+    class User
+    {
+        private int UserID { get; set; }
+
+        private string Username { get; set; }
+
+        private string Password { get; set; }
+
+        private UserType Type { get; set; }
+    }
+}
