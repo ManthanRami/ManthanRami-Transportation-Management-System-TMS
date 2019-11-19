292451ce474dc71f8b1111cb2bef702c5ce5dd25
Alex MacCumber <Alexander.MacCumber@outlook.com>
2019-11-18 20:30:42 -0500

    Setup initial login screen for the TMS Project. Login button has simple functionality to store the the username and password in local variables.  Size of the login screen is currently a set size, that cannot be changed by the user through dragging the edges

---
+++ Login_Window/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TMS
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected string username;
+        protected string password;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            username = UserNameBox.Text;
+            password = PasswordBox.Password;
+        }
+    }
+}
