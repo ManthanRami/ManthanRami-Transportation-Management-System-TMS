12a7213e8034db6ea6d23932970b8983083a0ec4
Sniddunc <sniddunc2@gmail.com>
2019-11-21 13:16:08 -0500

    :construction: Added exception for a primary key ID query fail

---
+++ TMS/Exceptions/CouldNotGetIdException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Exceptions
+{
+    public class CouldNotGetIdException : Exception
+    {
+        public CouldNotGetIdException()
+        {
+        }
+
+        public CouldNotGetIdException(string message) : base(message)
+        {
+        }
+
+        public CouldNotGetIdException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
