3475c99a4bbc3909bc7d88a4a4310d25275a81c9
Sniddunc <sniddunc2@gmail.com>
2019-11-21 12:56:08 -0500

    :construction: Created CarrierNotExistsException

---
+++ TMS/Exceptions/CarrierNotExistsException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Exceptions
+{
+    public class CarrierNotExistsException : Exception
+    {
+        public CarrierNotExistsException()
+        {
+        }
+
+        public CarrierNotExistsException(string message) : base(message)
+        {
+        }
+
+        public CarrierNotExistsException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
