d913fbd118a5528c3fb54cde594db3fb65817ca3
Sniddunc <sniddunc2@gmail.com>
2019-11-21 12:27:45 -0500

    :card_file_box: Created the Carrier class to model the Carrier table

---
+++ TMS/Data/Carrier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Data
+{
+    public class Carrier
+    {
+        public uint CarrierID { get; set; }
+        public City DepotCity { get; set; }
+
+        public int FtlAvailability { get; set; }
+        public int LtlAvailability { get; set; }
+    }
+}
