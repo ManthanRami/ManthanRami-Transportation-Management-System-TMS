ff9748176bba83d4d2d5d8fecb03421646ee9537
Sniddunc <sniddunc2@gmail.com>
2019-11-26 10:50:53 -0500

    :construction: Added Logger class

---
+++ TMS/Utils/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Utils
+{
+    public enum LogLevel
+    {
+        INFO,
+        WARN,
+        ERROR
+    }
+
+    public enum LogType
+    {
+        UI,
+        DATABASE,
+        AUTH
+    }
+
+    public class Logger
+    {
+        public static void Info(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Warn(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
