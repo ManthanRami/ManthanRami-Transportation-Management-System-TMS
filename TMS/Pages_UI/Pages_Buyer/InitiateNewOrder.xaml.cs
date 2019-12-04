7c61f7d59b3d972f9851bd552f05caf748ec3c8a
Manthan Rami <ramimanthan1999@outlook.com>
2019-12-04 03:39:01 -0500

    Behind code logic added for contract

--- TMS/Pages_UI/Pages_Buyer/InitiateNewOrder.xaml.cs
+++ TMS/Pages_UI/Pages_Buyer/InitiateNewOrder.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using TMS.Data;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace TMS.Pages_UI.Pages_Buyer
 {
@@ -20,9 +22,41 @@ namespace TMS.Pages_UI.Pages_Buyer
     /// </summary>
     public partial class InitiateNewOrder : Page
     {
+        CmpDal cmp;
+        List<Contract> crt;
         public InitiateNewOrder()
         {
             InitializeComponent();
+        }
+
+        private void INI_Click(object sender, RoutedEventArgs e)
+        {
+            cmp = new CmpDal();
+            if(cmp!=null)
+            {
+                connectSignal.Fill = Brushes.LightGreen;
+            }
+        }
+
+        private void Getorder_Click(object sender, RoutedEventArgs e)
+        {
+            cmp = new CmpDal();
+            crt = new List<Contract>();
+            crt = cmp.GetContracts();
+            contractData.ItemsSource = crt;
+        }
+
+        private void contractData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid gd = (DataGrid)sender;
+            dynamic rowView = gd.SelectedItem;           
+            if (rowView!=null)
+            {
+                ClientName.Text = rowView.Client;
+                JoBType.Text = rowView.JobType.ToString();
+                vanType.Text = rowView.VanType.ToString();
+                Quantity.Text = rowView.Quantity.ToString();
+            }
         }
     }
 }
