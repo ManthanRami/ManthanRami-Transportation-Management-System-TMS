99005bf17d927e8950b5e46c2ea4d66a745e4caf
Manthan Rami <ramimanthan1999@outlook.com>
2019-12-09 14:37:35 -0500

    Minor changes

--- TMS/Pages_UI/Pages_Admin/ModifyCarrierData.xaml.cs
+++ TMS/Pages_UI/Pages_Admin/ModifyCarrierData.xaml.cs
@@ -39,7 +39,7 @@ namespace TMS.Pages_UI.Pages_Admin
         private void UpdateCarrier(object sender, RoutedEventArgs e)
         {
             carrier.Name = txtCarrier_ID.Text;
-            City city = (City)Enum.Parse(typeof(City), txtDepot_City.Text);
+            TMS.Data.City city = (TMS.Data.City)Enum.Parse(typeof(TMS.Data.City), txtDepot_City.Text);
             carrier.DepotCity = city;
             carrier.FtlAvailability = Convert.ToInt32(txtFTL_Avail.Text);
             carrier.LtlAvailability = Convert.ToInt32(txtLTL_Avail.Text);
