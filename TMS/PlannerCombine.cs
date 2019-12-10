6bfd444dde9c776479c89b1181d137dba08820a2
David <frozentano@gmail.com>
2019-12-09 21:08:42 -0500

    comment updates

--- TMS/PlannerCombine.cs
+++ TMS/PlannerCombine.cs
@@ -18,20 +18,21 @@ namespace TMS
     public class PlannerCombine
     {
         static int ftlMax = 26; //only 26 skids on a truck
-        public int ftlTrucks;
-        public int ltlPallets;
-
+        public int ftlTrucks;   //num of Trucks
+        public int ltlPallets;  //num of ltl pallets
+        //Figure out the cost effective count of FTL trucks and LTL pallets
         public PlannerCombine(double ftlRate, double ltlRate, int pallets)
         {
             ftlTrucks = 0;
             ltlPallets = 0;
             int ftlMin = 0;
+            //determine minimum number of pallets to make ordering a FTL cost effective
             while ((ltlRate * ftlMin) < ftlRate)
             {
                 ftlMin++;
             }
             ftlMin--;
-
+            //Determine the number of LTL pallets and FTL trucks
             while (pallets > 0)
             {
                 if (pallets >= ftlMin)
